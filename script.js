da31c5cb3fb84b2cfabfc78d88bcccc5acd84837
ael-n <edwidgearie@gmail.com>
2026-03-27 14:37:25 -0700

    assurer que rien ne deborde

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* */
 
-const divContainer = document.querySelector("#container");
+const container = document.querySelector("#container");
 createGrid(3, container);
 hover()
 
@@ -23,17 +23,22 @@ button.addEventListener("click", event => {
 /* Fonction qui cree la grille */
 
 function createGrid(gridPerside, container){
+    let containerWidth = parseInt(window.getComputedStyle(container).width);
+    let totalBorderWidth = 2 + 2; 
+    let safetyLimit = 0.95; 
 
-    for (let i = 0; i <= gridPerside; i++) {
-        for (let j = 0; j <= gridPerside; j++) {
+    for (let i = 0; i < gridPerside; i++) {
+        for (let j = 0; j < gridPerside; j++) {
             let grid = document.createElement("div");
             grid.classList.add("grid");
-            container.appendChild(grid);
         }
     }
 
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     grid.style.background = "#" + randomColor;
+    grid.style.oppacity = 0;
+
+    container.appendChild(grid);
 
 
 }
