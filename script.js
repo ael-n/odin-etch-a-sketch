29e128837bae6449c8b2bfa7e7eed4def92b0412
ael-n <edwidgearie@gmail.com>
2026-03-27 13:44:00 -0700

    create vent for the button

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 /* */
 
-const divContainer = document.querySelector("container");
+const divContainer = document.querySelector("#container");
+craeteGrid(3, container);
+hover()
+
+/* Creer une nouvelle grille */
+
 const button = document.querySelector("button");
+button.addEventListener("click", event => {
+    let gridPerSide = prompt(`Enter number of grid per side; `);
+    if (gridPerside > 100) {
+        alert("Invalid number! Please enter a number between 1 and 100.");
+        return;
+    }
+    removeGrid(container);
+    createGrid(gridPerSide, container);
+    hover();
+
+})
+
+
+/* Fonction qui cree la grille */
+
+function createGrid(gridPerside, container){
+
+}
 
 
 
